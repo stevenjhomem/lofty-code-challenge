a179b5adcedc089b06ba70a591283c78153805f0
stevenjhomem <sjhomem@outlook.com>
2023-03-15 08:32:25 -0500

    Updating the increment command to be a bit more sleek.

--- lofty_project/keys/management/commands/increment_the_incremented.py
+++ lofty_project/keys/management/commands/increment_the_incremented.py
@@ -1,7 +1,6 @@
 import time
 
 from django.core.management.base import BaseCommand
-from django.db.models import F
 
 from keys.models import Key
 
@@ -12,7 +11,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         while True:
             self.stdout.write('Incrementing service has begun.')
-            queryset = Key.objects.filter(value__gt=0).values_list('id', flat=True)
-            number_updated = queryset.bulk_update(value=F("value") + 1, batch_size=300)  # bulk update is an atomic transaction now.
-            self.stdout.write(f'Incrementing service has finished. Updated {number_updated} of keys. Sleeping for 6 seconds.')
-            time.sleep(6)
+            keys = Key.objects.filter(value__gt=0)
+            for key in keys:
+                key.value += 1
+            Key.objects.bulk_update(keys, ['value'])  # bulk update is an atomic transaction now. (https://github.com/django/django/blob/stable/4.0.x/django/db/models/query.py#L640)
+            self.stdout.write(f'Incrementing service has finished. Updated {keys.count()} keys. Sleeping for 60 seconds.')
+            time.sleep(60)
